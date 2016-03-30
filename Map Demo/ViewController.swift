e7986124a9a96db5f1f9f10dcfe174c4a74435fe
Casey <Code@casey-macbook.local>
2016-03-29 21:07:33 -0700

    Initial Commit

---
+++ Map Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map Demo
+//
+//  Created by Code on 3/29/16.
+//  Copyright © 2016 ccorvino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
